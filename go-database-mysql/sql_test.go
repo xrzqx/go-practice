17d57b036dd2e18030818cb8c23c588d57a0e764
xrzq <abdrzq84@gmail.com>
2022-10-15 00:55:41 +0700

    add TestQueryCtx

--- go-database-mysql/sql_test.go
+++ go-database-mysql/sql_test.go
@@ -21,3 +21,32 @@ func TestExecSql(t *testing.T)  {
 	fmt.Println("Inserted new User")
 
 }
+
+func TestQueryCtx(t *testing.T)  {
+	db:= GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	// sql_script := "SELECT idUser, Name FROM User"
+	sql_script := "SELECT * FROM User"
+	rows, err := db.QueryContext(ctx, sql_script)
+	
+	if err != nil{
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next(){
+		var idUser, Name string
+		err = rows.Scan(&idUser, &Name)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Println("id: ", idUser)
+		fmt.Println("Name: ", Name)
+	}
+	
+
+}
